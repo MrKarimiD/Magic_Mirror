270a57d26722a0441497a55b16aefc43391b64af
MrKarimiD <mr.karimi.d@gmail.com>
2018-04-01 15:28:37 +0900

    Open the Camera

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,35 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    camTimer = new QTimer();
+    connect(camTimer, SIGNAL(timeout()), this, SLOT(cam_timeout()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    bool isOpened = cap.open(0);
+    if( !isOpened )  // check if we succeeded
+    {
+        qDebug()<<"cannot open";
+        return;
+    }
+    cap.open(0);
+    camTimer->start(30);
+}
+
+void MainWindow::cam_timeout()
+{
+    Mat frame;
+    cap >> frame;
+    cv::resize(frame,frame,Size(640,480),0,0,INTER_CUBIC);
+    cvtColor(frame, frame, COLOR_BGR2RGB);
+    QImage imgIn;
+    imgIn= QImage((uchar*) frame.data, frame.cols, frame.rows, frame.step, QImage::Format_RGB888);
+    ui->image_label->setPixmap(QPixmap::fromImage(imgIn));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,17 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDebug>
+#include <QTimer>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
+using namespace cv;
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +26,14 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+    void cam_timeout();
+
 private:
     Ui::MainWindow *ui;
+    QTimer *camTimer;
+    VideoCapture cap;
 };
 
 #endif // MAINWINDOW_H
