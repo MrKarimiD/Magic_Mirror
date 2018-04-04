42736a294f6c53109953137caa123dcc7720c2f0
MrKarimiD <mr.karimi.d@gmail.com>
2018-04-04 17:04:47 +0900

    Add filter, it should be adjust to Halla's face

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,6 +11,10 @@ MainWindow::MainWindow(QWidget *parent) :
 
     camTimer = new QTimer();
     connect(camTimer, SIGNAL(timeout()), this, SLOT(cam_timeout()));
+
+    QStringList output_items;
+    output_items<<"Input"<<"Output";
+    ui->output_comboBox->addItems(output_items);
 }
 
 MainWindow::~MainWindow()
@@ -26,6 +30,7 @@ void MainWindow::on_pushButton_clicked()
         qDebug()<<"cannot open";
         return;
     }
+    ui->output_comboBox->setEnabled(true);
     cap.open(0);
     camTimer->start(30);
 }
@@ -36,9 +41,16 @@ void MainWindow::cam_timeout()
     cap >> frame;
 
     Mat face = ofi->detectingLandmarks(frame);
-    cv::resize(face,face,Size(640,480),0,0,INTER_CUBIC);
-    cvtColor(face, face, COLOR_BGR2RGB);
+
+    Mat output;
+    if(ui->output_comboBox->currentText() == "Output")
+        face.copyTo(output);
+    else
+        frame.copyTo(output);
+
+    cv::resize(output,output,Size(640,480),0,0,INTER_CUBIC);
+    cvtColor(output, output, COLOR_BGR2RGB);
     QImage imgIn;
-    imgIn= QImage((uchar*) face.data, face.cols, face.rows, face.step, QImage::Format_RGB888);
+    imgIn= QImage((uchar*) output.data, output.cols, output.rows, output.step, QImage::Format_RGB888);
     ui->image_label->setPixmap(QPixmap::fromImage(imgIn));
 }
--- openfaceinterface.cpp
+++ openfaceinterface.cpp
@@ -9,74 +9,26 @@ OpenFaceInterface::OpenFaceInterface(QObject *parent)
     facemark->loadModel("lbfmodel.yaml");
 }
 
-static bool myDetector(InputArray image, OutputArray faces, CascadeClassifier *face_cascade)
-{
-    Mat gray;
-
-    if (image.channels() > 1)
-        cvtColor(image, gray, COLOR_BGR2GRAY);
-    else
-        gray = image.getMat().clone();
-
-    equalizeHist(gray, gray);
-
-    std::vector<Rect> faces_;
-    face_cascade->detectMultiScale(gray, faces_, 1.4, 2, CASCADE_SCALE_IMAGE, Size(30, 30));
-    Mat(faces_).copyTo(faces);
-    return true;
-}
-
 Mat OpenFaceInterface::detectingLandmarks(Mat input)
 {
-    /*string filename = "lbfmodel.yaml";
-    string cascade_name = "haarcascade_frontalface_alt.xml";
-
-    Mat img;
-    input.copyTo(img);
-
-    //pass the face cascade xml file which you want to pass as a detector
-    CascadeClassifier face_cascade;
-    qDebug()<<"line1";
-    face_cascade.load(cascade_name);
-    qDebug()<<"line2";
-    FacemarkKazemi::Params params;
-    Ptr<FacemarkKazemi> facemark = FacemarkKazemi::create(params);
-    qDebug()<<"line3";
-    facemark->setFaceDetector((FN_FaceDetector)myDetector, &face_cascade);
-    qDebug()<<"line4";
-    facemark->loadModel(filename);
-    qDebug()<<"line5";
-    cout<<"Loaded model"<<endl;
-    vector<Rect> faces;
-    resize(img,img,Size(460,460), 0, 0, INTER_LINEAR_EXACT);
-    qDebug()<<"line6";
-    facemark->getFaces(img,faces);
-    vector< vector<Point2f> > shapes;
-    if(facemark->fit(img,faces,shapes))
-    {
-        for( size_t i = 0; i < faces.size(); i++ )
-        {
-            cv::rectangle(img,faces[i],Scalar( 255, 0, 0 ));
-        }
-        for(unsigned long i=0;i<faces.size();i++){
-            for(unsigned long k=0;k<shapes[i].size();k++)
-                cv::circle(img,shapes[i][k],5,cv::Scalar(0,0,255),FILLED);
-        }
-    }
-    */
-
     // Variable to store a video frame and its grayscale
     Mat frame, gray;
     input.copyTo(frame);
+    Mat out(input.rows, input.cols, CV_8UC3, Scalar(0,0,0));
 
     // Find face
     vector<Rect> faces;
     // Convert frame to grayscale because
     // faceDetector requires grayscale image.
+    medianBlur ( frame, frame, 7 );
     cvtColor(frame, gray, COLOR_BGR2GRAY);
 
     // Detect faces
     faceDetector.detectMultiScale(gray, faces);
+    for(int i = 0; i< faces.size(); i++)
+    {
+        qDebug()<<"face "<<i<<" : "<<faces.at(i).width << " , "<<faces.at(i).height;
+    }
 
     // Variable for landmarks.
     // Landmarks for one face is a vector of points
@@ -89,20 +41,11 @@ Mat OpenFaceInterface::detectingLandmarks(Mat input)
 
     if(success)
     {
-        // If successful, render the landmarks on the face
-//        for(int i = 0; i < landmarks.size(); i++)
-//        {
-//            drawLandmarks(frame, landmarks[i]);
-//        }
-
         for(unsigned long i=0;i<faces.size();i++){
             for(unsigned long k=0;k<landmarks[i].size();k++)
-                cv::circle(frame,landmarks[i][k],5,cv::Scalar(0,0,255),FILLED);
+                cv::circle(out,landmarks[i][k],5,cv::Scalar(0,0,255),FILLED);
         }
     }
 
-    // Display results
-    //imshow("Facial Landmark Detection", frame);
-
-    return frame;
+    return out;
 }
